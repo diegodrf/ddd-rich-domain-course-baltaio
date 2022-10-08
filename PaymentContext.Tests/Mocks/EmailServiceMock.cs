65ad1b0787c17597b49690f4f2372434481cb790
Diego Faria <diego.rdfaria@gmail.com>
2022-10-08 02:10:27 -0300

    Add Mocks

---
+++ PaymentContext.Tests/Mocks/EmailServiceMock.cs
@@ -0,0 +1,17 @@
+﻿using PaymentContext.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Tests.Mocks
+{
+    public class EmailServiceMock : IEmailService
+    {
+        public void SendEmail(string to, string from, string subject, string body)
+        {
+            
+        }
+    }
+}
---
+++ PaymentContext.Tests/Mocks/StudentRepositoryMock.cs
@@ -0,0 +1,27 @@
+﻿using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Repositories;
+
+namespace PaymentContext.Tests.Mocks
+{
+    public class StudentRepositoryMock : IStudentRepository
+    {
+        public void Create(Student student)
+        {
+            
+        }
+
+        public bool DocumentExists(string document)
+        {
+            if(document.StartsWith("00000000000"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool EmailExists(string email)
+        {
+            return false;
+        }
+    }
+}
